e6577b6999c884830a932ba311bb3c691ac46d8b
tdkhoa1212 <tdkhoa1212@gmail.com>
2024-04-27 20:06:34 +0700

    27/04/2024

--- app/services/Visualization_service.py
+++ app/services/Visualization_service.py
@@ -12,7 +12,18 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 def generate_labels_plot(data, fpt):
+    # 122, 490, 2537
     try:
+        if len(data) == 122:
+            distance = 12
+        elif len(data) == 490:
+            distance = 30
+        elif len(data) == 2537:
+            distance = 200
+        else:
+            # Set a default distance if the length of data doesn't match any condition
+            distance = 10  # Adjust this value as needed
+
         True_labels = np.concatenate((np.ones(fpt,), np.linspace(fpt, 0, num=len(data) - fpt) / fpt)) * 100
         Predicted_labels = True_labels + np.concatenate((np.random.uniform(-0.02, 0, size=fpt + 5),
                                                          np.random.uniform(-0.03, 0.03, size=len(True_labels) - fpt - 5))) * 100
@@ -34,21 +45,21 @@ def generate_labels_plot(data, fpt):
                 if line.get_label() in ['Predicted RUL', 'Predicted RUL line']:
                     line.remove()
             for anno in ax.texts:
-                if anno.get_text() == f'Predicted RUL: {round(Predicted_labels[frame-1], 2)}%':
+                if anno.get_text().startswith('Health State'):
                     anno.remove()
 
             ax.axvline(x=frame + 1, color='white', linestyle='--', linewidth=1, label='Predicted RUL line')  
             if frame < len(Predicted_labels) - 1:  # Check if it's not the last frame
-                ax.annotate(f'Predicted RUL: {round(Predicted_labels[frame], 2)}%', xy=(frame + 1, Predicted_labels[frame]), xytext=(frame + 1 + 5, Predicted_labels[frame] + 10),
+                ax.annotate(f'Health State: {round(Predicted_labels[frame], 2)}%', xy=(frame + 1, Predicted_labels[frame]), xytext=(frame + 1 + 5, Predicted_labels[frame] + 10),
                             arrowprops=dict(facecolor='white', arrowstyle='->'), color='white')  
             predicted_point = ax.plot(range(frame + 1), Predicted_labels[:frame + 1], 'wo', markersize=2, label='Predicted RUL')
             return predicted_point
 
-        anim = FuncAnimation(fig, update, frames=len(Predicted_labels), interval=200, blit=True)
+        anim = FuncAnimation(fig, update, frames=range(0, len(Predicted_labels), distance), interval=200, blit=True)
         ax.legend(labels=["True RUL"], loc='lower left')
 
         video_path = 'static/videos/animation.mp4'
-        anim.save(video_path, writer='ffmpeg', fps=10, bitrate=1000000, dpi=300)  # Adjust bitrate and dpi for maximum sharpness        
+        anim.save(video_path, writer='ffmpeg', fps=2, bitrate=1000000, dpi=300)    
         plt.close()
 
         return HTML(f'<video controls autoplay loop muted width="800" height="600"><source src="{video_path}" type="video/mp4"></video>'), Predicted_labels
